78d8f662b34ee206802c60345e5a2f7e58a58a28
laureanomarenco <laureanomarenco@gmail.com>
2022-11-01 18:07:19 -0300

    feat/addToCart-function

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { Store } from "../utils/Store";
 
 export default function Layout({ title, children }) {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <>
       <Head>
@@ -17,13 +20,26 @@ export default function Layout({ title, children }) {
               <span className="text-lg font-bold">Next</span>
             </Link>
             <div>
-              <Link href="/cart"><span className="p-4">Cart</span></Link>
-              <Link href="/login"><span className="p-4">Login</span></Link>
+              <Link href="/cart">
+                <span className="p-4">
+                  Cart{" "}
+                  {cart.cartItems.length > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </span>
+                  )}
+                </span>
+              </Link>
+              <Link href="/login">
+                <span className="p-4">Login</span>
+              </Link>
             </div>
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex h-10 justify-center items-center shadow-inner">Thanks to be here - Next e-commerce</footer>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          Thanks to be here - Next e-commerce
+        </footer>
       </div>
     </>
   );
---
+++ utils/Store.js
@@ -0,0 +1,32 @@
+import { createContext, useReducer } from "react";
+
+export const Store = createContext();
+
+const initialState = {
+  cart: { cartItems: [] },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CART_ADD_ITEM": {
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (i) => i.slug === newItem.slug
+      );
+      const cartItems = existItem
+        ? state.cart.cartItems.map((i) =>
+            i.name === existItem.name ? newItem : i
+          )
+        : [...state.cart.cartItems, newItem];
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+    default:
+      return state;
+  }
+}
+
+export function StoreProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{children}</Store.Provider>;
+}
