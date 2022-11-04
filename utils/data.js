3e7b2113bf464bfd558bcea3809b035beeb4408a
laureanomarenco <laureanomarenco@gmail.com>
2022-11-04 15:47:45 -0300

    feat/moongoDB-connect

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Admin',
+            email: 'laureanomarenco@gmail.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true
+        },
+        {
+            name: 'Test',
+            email: 'falsomail@gmail.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false
+        }
+    ],
     products: [
         {
             name: 'Medallones de Quinoa',
