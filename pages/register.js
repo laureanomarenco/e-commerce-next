4e0ca0faa92c50ee9644f814bf968b60cc262b4f
laureanomarenco <laureanomarenco@gmail.com>
2022-11-08 12:53:41 -0300

    feat/register

--- pages/register.js
+++ pages/register.js
@@ -6,6 +6,7 @@ import { signIn, useSession } from "next-auth/react";
 import { getError } from "../utils/error";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 export default function LoginScreen() {
 
@@ -23,11 +24,18 @@ export default function LoginScreen() {
   const {
     handleSubmit,
     register,
+    getValues,
     formState: { errors },
   } = useForm();
 
-  const submitHandler = async ({ email, password }) => {
+  const submitHandler = async ({ name, email, password }) => {
     try {
+      await axios.post('/api/auth/signup', {
+        name, 
+        email,
+        password,
+      })
+
       const result = await signIn('credentials', {
         redirect: false,
         email,
@@ -42,12 +50,31 @@ export default function LoginScreen() {
   };
 
   return (
-    <Layout title="Login">
+    <Layout title="Register">
       <form
         className="mx-auto max-w-screen-md"
         onSubmit={handleSubmit(submitHandler)}
       >
-        <h1 className="mb-4 text-xl">Login</h1>
+        <h1 className="mb-4 text-xl">Create Account</h1>
+        <div className="mb-4">
+          <label htmlFor="name">Name</label>
+          <input
+            type="name"
+            {...register("email", {
+              required: "Please enter name",
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]+.[a-zA-Z0-9_.+-]+$/i,
+                message: "Please enter valid name",
+              },
+            })}
+            className="w-full"
+            id="name"
+            autoFocus
+          ></input>
+          {errors.name && (
+            <div className="text-red-500">{errors.name.message}</div>
+          )}
+        </div>
         <div className="mb-4">
           <label htmlFor="email">Email</label>
           <input
@@ -61,7 +88,6 @@ export default function LoginScreen() {
             })}
             className="w-full"
             id="email"
-            autoFocus
           ></input>
           {errors.email && (
             <div className="text-red-500">{errors.email.message}</div>
@@ -83,11 +109,31 @@ export default function LoginScreen() {
             <div className="text-red-500">{errors.password.message}</div>
           )}
         </div>
-        <div>
-          <button className="primary-button">Login</button>
+        <div className="mb-4">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="confirmPassword"
+            {...register("password", {
+              required: "Please enter confirm password",
+              validate: (value) => value === getValues('password'),
+              minLength: { value: 6, message: "confirm password is more than 5 chars" },
+            })}
+            className="w-full"
+            id="confirmPassword"
+            autoFocus
+          ></input>
+          {errors.confirmPassword && (
+            <div className="text-red-500">{errors.confirmPassword.message}</div>
+          )}
+          {errors.confirmPassword && errors.confirmPassword.type === 'validate' &&(
+            <div className="text-red-500">Password do not match</div>
+          )}
         </div>
         <div>
-        Dont have an account? <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
+          <button className="primary-button">Register</button>
+        </div>
+        <div>
+          Dont have an account? <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
         </div>
       </form>
     </Layout>
